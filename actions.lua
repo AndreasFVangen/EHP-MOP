d81219c08745d18424af27534cda32b2b1e62e00
AndreasFVangen <35685681+AndreasFVangen@users.noreply.github.com>
2022-04-17 11:23:30 +0200

    Create actions.lua

---
+++ actions.lua
@@ -0,0 +1,57 @@
+-- SETUP --
+aura_env.player_class = UnitClass("player")
+
+-- INIT STAGGER --
+aura_env.stagger = 0
+if aura_env.player_class == "Monk" then
+    aura_env.stagger = 0.2 + GetMasteryEffect()/100
+end
+
+
+aura_env.class_reductions = {
+    ["Warrior"] = {
+        
+    },
+    ["Paladin"] = {
+        
+    },
+    ["Hunter"] = {
+        
+    },
+    ["Rogue"] = {
+        
+    },
+    ["Priest"] = {
+        
+    },
+    ["Shaman"] = {
+        
+    },
+    ["Mage"] = {
+        
+    },
+    ["Monk"] =  {
+        Buff:new{"Shuffle", 0, 0 , 0.2},
+        Buff:new{"Fortifying Brew", 0.25, 0.25, 0.2},
+        Buff:new{"Diffuse Magic", 0, 0.9, 0},
+        Buff:new{"Zen Meditation", 0.9, 0.9, 0}
+    },
+    ["Warlock"] = {
+        Buff:new{"Unending Resolve", 0.4, 0.4, 0},
+        Buff:new{"Soul Link", 0.2, 0.2, 0}
+        
+    },
+    ["Druid"] = {
+        
+    },
+    ["Death Knight"] = {
+        
+    }
+    
+}
+
+aura_env.global_reductions = {
+    Buff:new{"Avert Harm", 0.2, 0.2, 0},
+    Buff:new{"Barkskin", 0.2, 0.2, 0},
+}
+
