bad1daed6d7e5e7a6d7dda945e43f45cc2cb7630
featiire <32174193+featiire@users.noreply.github.com>
2022-04-17 18:40:57 +0200

    Update actions.lua

--- actions.lua
+++ actions.lua
@@ -12,12 +12,12 @@ end
 aura_env.class_reductions = {
     ["Warrior"] = {
         Buff:new{"Shield Wall", 0.4, 0.4, 0},
-		Buff:new{"Die by The Sword", 0.2, 0.2, 0},
+	Buff:new{"Die by The Sword", 0.2, 0.2, 0},
     },
     ["Paladin"] = {
         Buff:new{"Divine Protection", 0.2, 0.2, 0},
-		Buff:new{"Guardian of Ancient Kings", 0.5, 0.5, 0},
-		Buff:new{"Ardent Defender", 0.2, 0.2, 0},
+	Buff:new{"Guardian of Ancient Kings", 0.5, 0.5, 0},
+	Buff:new{"Ardent Defender", 0.2, 0.2, 0},
     },
     ["Hunter"] = {
         Buff:new{"Deterrence", 0.3, 0.3, 0},
@@ -30,18 +30,18 @@ aura_env.class_reductions = {
     },
     ["Shaman"] = {
         Buff:new{"Shamanistic Rage", 0.3, 0.3, 0},
-		Buff:new{"Astral Shift", 0.4, 0.4, 0},
+	Buff:new{"Astral Shift", 0.4, 0.4, 0},
     },
     ["Mage"] = {
         Buff:new{"Greater Invisibility", 0.9, 0.9, 0},
-		Buff:new{"Temporal Shield", 0.15, 0.15, 0},
+	Buff:new{"Temporal Shield", 0.15, 0.15, 0},
     },
     ["Monk"] =  {
         Buff:new{"Shuffle", 0, 0, 0.2},
         Buff:new{"Fortifying Brew", 0.25, 0.25, 0.2},
         Buff:new{"Diffuse Magic", 0, 0.9, 0},
         Buff:new{"Zen Meditation", 0.9, 0.9, 0}
-		Buff:new{"Dampen Harm", 0.5, 0.5, 0},
+	Buff:new{"Dampen Harm", 0.5, 0.5, 0},
     },
     ["Warlock"] = {
         Buff:new{"Unending Resolve", 0.4, 0.4, 0},
@@ -49,11 +49,11 @@ aura_env.class_reductions = {
     },
     ["Druid"] = {
         Buff:new{"Barkskin", 0.2, 0.2, 0},
-		Buff:new{"Survival Instincts", 0.5, 0.5, 0},
+	Buff:new{"Survival Instincts", 0.5, 0.5, 0},
     },
     ["Death Knight"] = {
         Buff:new{"Icebound Fortitude", 0.2, 0.2, 0},
-		Buff:new{"Bone Shield", 0.2, 0.2, 0},
+	Buff:new{"Bone Shield", 0.2, 0.2, 0},
     }
     
 }
@@ -64,7 +64,7 @@ aura_env.global_reductions = {
     Buff:new{"Power Word: Barrier", 0.25, 0.25, 0},
     Buff:new{"Pain Suppression", 0.4, 0.4, 0},
     Buff:new{"Aura Mastery", 0.2, 0.2, 0},
-	Buff:new{"Vigilance", 0.4, 0.4, 0},
-	Buff:new{"Hand of Sacrifice", 0.3, 0.3, 0},
+    Buff:new{"Vigilance", 0.4, 0.4, 0},
+    Buff:new{"Hand of Sacrifice", 0.3, 0.3, 0},
 }
 
